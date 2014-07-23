a966847973f4803b9404d6088d2d16bd3ef9a484
Klemen Kenda <klemen.kenda@ijs.si>
2014-07-23 14:24:06 +0200

    Cleanup ...

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({port: 8080});
+// configuration
+var port = 8080;
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: port });
 
+// web socket server - broadcast function
 wss.broadcast = function(data) {
     for(var i in this.clients)
         this.clients[i].send(data);
 };
 
+// web socket server
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
-        console.log('received: %s', message);
-	wss.broadcast(message);
+        console.log('Received&broadcasted: %s', message);
+	    wss.broadcast(message);
     });    
 });
